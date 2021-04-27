e578689667b9ee4224c9ccfde9336378f80fa883
ty69 <IceScripterValid+Github@gmail.com>
2021-04-27 06:04:47 -0700

    Added ui control

--- Scripts/Vesteria.lua
+++ Scripts/Vesteria.lua
@@ -4,13 +4,19 @@ end
 rconsolename'Ulisse'
 local FireServer=Instance.new'RemoteEvent'.FireServer
 local OldFS
+local GodMode=false
 OldFS=hookfunction(FireServer, newcclosure(function(Self, ...)
 	if not checkcaller() then
 		local Args={...}
-		if Args and Args[3] and typeof(Args[3])=='string' and Args[3]=='monster' then
-			rconsoleprint'Monster damage called, returning\n'
+		if Args and Args[3] and typeof(Args[3])=='string' and Args[3]=='monster' and GodMode then
 			return
 		end
 	end
 	return OldFS(Self,...)
 end))
+local UI=UlisseUI:Main()
+local Tab=UI:Tab'Tab 1'
+local Section=Tab:Section'Section'
+Section:Item('toggle','Monster Godmode',function(v)
+    GodMode=v
+end)
